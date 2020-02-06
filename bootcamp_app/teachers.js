e29075582c8e66d3a73c5724b53de9d8c2c73f8b
antosha-85 <antosha-85@yandex.ru>
2020-02-06 04:01:21 +0000

    Finished categorized search

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,15 +8,18 @@ const pool = new Pool({
 });
 const args = process.argv.slice(2);
 
-pool.query(`
-    SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort 
-    FROM teachers 
-    JOIN assistance_requests ON teachers.id = teacher_id
-    JOIN students ON student_id = students.id 
-    JOIN cohorts ON cohort_id = cohorts.id
-    WHERE cohorts.name = '${args[0] || 'JUL02'}'
-    ORDER BY teacher;
-`)
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort 
+FROM teachers 
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON student_id = students.id 
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher;`
+
+const cohortName = args[0] || 'JUL02';
+const values = [cohortName];
+
+pool.query(queryString, values)
 .then(res => {
     console.log(`connected`);
   res.rows.forEach(user => {
